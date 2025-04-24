20a8397f9d4123978a6c788ae520d03b315ed8c1
Chimoneg27 <chimonegarvin27@gmail.com>
2025-04-24 23:01:37 +0200

    get individual projects

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-const express = require('express') //import express which is a function for making an express application
+const express = require("express"); //import express which is a function for making an express application
 // which is stored in the app variable
-const app = express()
+const app = express();
 
 const projects = [
   {
-    id: '1',
-    name: 'Kids Heaven Book Store',
-    techStack: ['html', 'reactjs', 'tailwind'],
-    website: 'http://kidsheavenbooks',
-    picture: ['pic1', 'pic2'],
-    description: 'this is a great site to order books'
+    id: "1",
+    name: "Kids Heaven Book Store",
+    techStack: ["html", "reactjs", "tailwind"],
+    website: "http://kidsheavenbooks",
+    picture: ["pic1", "pic2"],
+    description: "this is a great site to order books",
   },
   {
-    id: '1',
-    name: 'Kids Heaven Book Store',
-    techStack: ['html', 'reactjs', 'tailwind'],
-    website: 'http://kidsheavenbooks',
-    picture: ['pic1', 'pic2'],
-    description: 'this is a great site to order books'
+    id: "1",
+    name: "Kids Heaven Book Store",
+    techStack: ["html", "reactjs", "tailwind"],
+    website: "http://kidsheavenbooks",
+    picture: ["pic1", "pic2"],
+    description: "this is a great site to order books",
+  },
+];
+
+app.get("/", (request, response) => {
+  //original routes
+  response.send("<h1>Hello Garvin</h1");
+});
+
+app.get("/garvinchimone/projects", (request, response) => {
+  response.json(projects); // no need for stringify because in express the conversion happens automatically
+});
+
+app.get("/garvinchimone/projects/:id", (request, response) => {
+  const id = request.params.id; // access the id of the particuluar project
+  const project = projects.find((p) => p.id === id);
+  if (project) {
+    response.json(project);
+  } else {
+    response.status(404).end();
   }
-]
+});
 
-app.get('/', (request, response) => { //original routes
-  response.send('<h1>Hello Garvin</h1')
-})
-
-app.get('/garvinAPI/projects', (request, response) => {
-  response.json(projects) // no need for stringify because in express the conversion happens automatically
-})
-
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`)
-})
+  console.log(`Server running on ${PORT}`);
+});
