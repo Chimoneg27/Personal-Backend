db7a2b97dc3d422395295ac2eaa3e6fd5b51be35
Chimoneg27 <chimonegarvin27@gmail.com>
2025-05-09 22:20:43 +0200

    create blog endpoint

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get("/garvinchimone/projects/:id", async (request, response) => {
 
     response.json(project)
   } catch (error) {
-    response.status(500).json({ error: 'Faile to fetch project' })
+    response.status(500).json({ error: 'Failed to fetch project' })
   }
 });
 
@@ -63,6 +63,54 @@ app.post('/garvinchimone/projects', async (request, response) => {
     response.json(newProject)
   } catch (error) {
     response.status(500).json({ error: 'Failed to create project' })
+  }
+})
+
+app.get('garvinchimone/blogs', async (request, response) => {
+  try {
+    const blogs = await prisma.blog.findMany()
+    response.json(blogs)
+  } catch (error) {
+    response.status(500).json({ error: 'Failed to fetch blogs' })
+  }
+})
+
+app.get('/garvinchimone/blogs/:id', async (request, response) => {
+  const { id } = request.params
+
+  try {
+    const blog = await prisma.blog.findUnique({
+      where: { id: id }
+    })
+
+    if(!blog) return response.status(404).json({ error: 'Blog not found' })
+    
+    response.json(blog)
+  } catch (error) {
+    response.status(500).json({ error: 'Failed to fetch individual blog' })
+  }
+})
+
+app.post('/garvinchimone/projects', async (request, response) => {
+  const { title, content, author, image, tags } = request.body
+
+  if(!title || !author) {
+    return response.status(400).json({ error: 'Missing required fields' })
+  }
+
+  try {
+    const newBlog = await prisma.blog.create({
+      data: {
+        title,
+        content,
+        author,
+        image,
+        tags,
+      }
+    })
+    response.json(newBlog)
+  } catch (error) {
+    response.status(500).json({ error: 'Failed to post blog' })
   }
 })
 
