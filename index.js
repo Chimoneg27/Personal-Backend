f07e087fea4e3916bed79875c1c60df89065f532
Chimoneg27 <chimonegarvin27@gmail.com>
2025-05-09 22:00:10 +0200

    remove comments

--- index.js
+++ index.js
@@ -1,59 +1,12 @@
-const express = require("express"); //import express which is a function for making an express application
-// which is stored in the app variable
+const express = require("express");
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const app = express();
 const cors = require('cors')
 
-app.use(express.json()) // json parser. converts JSON into a usable data structure
-
-let projects = [
-  {
-    id: "1",
-    name: "Kids Heaven Book Store",
-    techStack: ["html", "reactjs", "tailwind"],
-    website: "http://kidsheavenbooks",
-    picture: 'pic1',
-    description: "this is a great site to order books",
-  },
-  {
-    id: "2",
-    name: "Kids Heaven Book Store",
-    techStack: ["html", "reactjs", "tailwind"],
-    website: "http://kidsheavenbooks",
-    picture: "pic 1",
-    description: "this is a great site to order books",
-  },
-];
-
-const main = async () => {
-  const newBlog = await prisma.blog.create({
-    data: {
-      title: 'General Overview',
-      content: `<p>This blog section of my personal portfolio will be for displaying my interests, current and future.</p><p>Expect to see content about my hobbies, random thoughts, and ideas for what's ahead.</p>`,
-      author: 'Garvin Chimone',
-      image: 'https://i.pinimg.com/736x/53/bd/a8/53bda8ac30f17d6cb250658167461dbe.jpg',
-      tags: ['Hobbies', 'Misc']
-    }
-  })
-
-  const allProjects = await prisma.project.findMany()
-  const allBlogs = await prisma.blog.findMany()
-  console.log(allBlogs)
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+app.use(express.json())
 
 app.get("/", (request, response) => {
-  //original routes
   response.send("<h1>Hello Garvin</h1");
 });
 
@@ -111,13 +64,6 @@ app.post('/garvinchimone/projects', async (request, response) => {
   } catch (error) {
     response.status(500).json({ error: 'Failed to create project' })
   }
-})
-
-app.delete('/garvinchimone/projects/:id', (request, response) => {
-  const id = request.params.id
-  projects = projects.filter(p => p.id !== id)
-
-  response.status(204).end()
 })
 
 const unknownEndpoint = (request, response) => {
